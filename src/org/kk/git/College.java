e4a062a6ec3902a69d2b66857c2d12263d933446
khube <khube@khube>
2019-05-09 15:59:56 +0530

    commit changes new variable added

--- src/org/kk/git/College.java
+++ src/org/kk/git/College.java
@@ -4,7 +4,18 @@ public class College {
 
 	private int collegeId;
 	private String collegeName;
+
+	private String course;
 	
+	
+	
+	
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
 	public int getCollegeId() {
 		return collegeId;
 	}
@@ -17,9 +28,13 @@ public class College {
 	public void setCollegeName(String collegeName) {
 		this.collegeName = collegeName;
 	}
-	
 	@Override
 	public String toString() {
-		return "College [collegeId=" + collegeId + ", collegeName=" + collegeName + "]";
+		return "College [collegeId=" + collegeId + ", collegeName=" + collegeName + ", course=" + course
+				+ ", getCourse()=" + getCourse() + ", getCollegeId()=" + getCollegeId() + ", getCollegeName()="
+				+ getCollegeName() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
 	}
+	
+
 }
